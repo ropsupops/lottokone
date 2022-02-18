255c3474e2ab87a6dd1fa74a6b12cb5e68e9bb85
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-02-18 11:40:29 +0200

    Lottokoneen alustavaa rakentelua

--- index.php
+++ index.php
@@ -18,17 +18,31 @@
         <input id="nappi" type="submit">
     </form>
     <?php
-        //ohjelma arpoo
-        
-
         //käyttäjä arvaa
         if (empty($_POST[1 or 2 or 3 or 4 or 5 or 6])) {
             include 'info.php';
         } else {
             if ($_POST[1 or 2 or 3 or 4 or 5 or 6] >= 1 and $_POST[1 or 2 or 3 or 4 or 5 or 6] <= 30) {
-               echo "hei"; 
-            }
-            else{
+                //ohjelma arpoo
+                $numbers=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
+        
+                /*
+            
+                array(
+                    $num1 = (array_rand($numbers, 1)),
+                    $num2 = (array_rand($numbers, 1)),
+                    $num3 = (array_rand($numbers, 1)),
+                    $num4 = (array_rand($numbers, 1)),
+                    $num5 = (array_rand($numbers, 1)),
+                    $num6 = (array_rand($numbers, 1)),
+                );
+
+                */
+
+                foreach ($numbers as $key => $value) {
+                    # code...
+                }
+            } else {
                 include 'info.php';
             }
         }
